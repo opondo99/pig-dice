aeb5a0ae7a7a1c5eef955ecedd67b7e67ba72c73
opondo99 <markopondo.m@gmail.com>
2018-07-21 17:24:49 +0300

    added content in the readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# pig-dice
+# PROJECT
+Pig dice game
+
+#AUTHOR
+Mark Opondo
+
+#DESCRIPTION
+This is a game where we have two players who are taking turns to roll dice. Each player gets as many rolls as they can but when they roll a one the turn goes to the other player. the goal is to score 100 points and whomever scores 100 points first wins rhe round.
+
+#SET UP INSTRUCTIONS
+All you need to play this game is internet a browser and a github account.
+
+#TOOLS USED
+*HTML
+*CSS
+*Bootstrap
+*JavaScript
+
+#BUGS 
+In case of any challenges you can contact me @markopondo.m@gmail.com
---
+++ js/script.js
@@ -0,0 +1,19 @@
+// $(document).ready(function(){
+//     $().(function(event){
+//         event.preventDefault()
+//         $("div#result").empty();
+
+// function rollDice() {
+//     var die1 = document.$("die1");
+//     var die2 = document.$("die2");
+//     var status = document.$("status");
+//     var d1 = math.floor(math.random() * 6) + 1;
+//     var d2 = math.floor(math.random() * 6) + 1;
+//     var getTotal = d1 + d2;
+//     die1.innerHTML = d1;
+//     die2.innerHTML = d2;
+//     status.innerHTML = "you rolled " +getTotal;
+//     if (d1 === d2){
+//         status.innerHTML = "You rolled a DOUBLE! You get a free turn!");
+//     }
+// });
