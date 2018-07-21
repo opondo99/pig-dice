5b6cdb90c5c705be938aeb3f6b636a6e249a004c
opondo99 <markopondo.m@gmail.com>
2018-07-22 00:22:13 +0300

    JavaScript hide show function has worked

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("rules");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
 // $(document).ready(function(){
 //     $().(function(event){
 //         event.preventDefault()
